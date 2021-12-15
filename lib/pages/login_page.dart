e55419c3758a876b405f8b9e362abca0300f10ee
PrarthanaB <prarthanab@winjit.com>
2021-12-15 15:38:56 +0530

    day5 Publish to Github,SingleChildScrollView,Navigator

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,61 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var size = Size(150, 40);
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset("assets/images/login_img.png", fit: BoxFit.cover),
-            const SizedBox(
-              height: 20.0,
-              //  child: Text("Hii prarthana")
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_img.png",
+                fit: BoxFit.cover,
+                height: 300,
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter username",
-                      labelText: "Username",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("Hii Prarthana..");
-                    },
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                  )
-                ],
+              const SizedBox(
+                height: 20.0,
+                //  child: Text("Hii prarthana")
               ),
-            )
-          ],
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: size),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
