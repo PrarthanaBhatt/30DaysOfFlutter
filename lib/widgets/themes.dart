b2a22a20c3b580650d875814ffe642d4d6c06c21
PrarthanaB <prarthanab@winjit.com>
2021-12-16 18:10:14 +0530

    day10 Models | AppBarTheme | Extracting Theme

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
@@ -10,9 +8,9 @@ class MyTheme {
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black), 
+        iconTheme: IconThemeData(color: Colors.black),
         toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
-         titleTextStyle: Theme.of(context).textTheme.headline6,
+        titleTextStyle: Theme.of(context).textTheme.headline6,
       ));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
