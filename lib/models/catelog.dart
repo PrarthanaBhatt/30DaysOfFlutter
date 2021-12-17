b584e589d20d062b384a2ed118b8935290f6ba6e
PrarthanaB <prarthanab@winjit.com>
2021-12-17 12:01:11 +0530

    day13 mapping json data manually

--- lib/models/catelog.dart
+++ lib/models/catelog.dart
@@ -1,5 +1,5 @@
 class CatalogModel {
-  static final items = [
+  static  List<Item> items = [
     Item(
         id: 1,
         name: "iPhone 12 Pro",
@@ -26,4 +26,27 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+//map to class, factory constructor when we want to initialize on basis of some logic
+// gives choice of which constructor to be applied
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+
+  //class to map
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,52 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catelog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 5;
-  final String name = "Prarthana";
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 5;
+
+  final String name = "Prarthana";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+      .map<Item>((item) => Item.fromMap(item))
+      .toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       body: ListView.builder(
         // itemCount: CatalogModel.items.length,
-        itemCount: dummyList.length,
+        itemCount: CatalogModel.items.length,
         itemBuilder: (context, index) {
           return ItemWidget(
-            item: dummyList[index],
+            item: CatalogModel.items[index],
           );
         },
       ),
